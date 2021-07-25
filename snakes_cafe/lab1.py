24ddd1a0e16d129043b1157b1b43db26fd9a32b7
FerasSadTawfiqEzz-aldeen <Ferasezaldeen@gmail.com>
2021-07-25 16:57:56 +0300

    finish lab1

---
+++ snakes_cafe/lab1.py
@@ -0,0 +1,57 @@
+from typing import Counter
+
+
+intro_msg=['**************************************','**    Welcome to the Snakes Cafe!   **','**    Please see our menu below.    **','**','** To quit at any time, type "quit" **','**************************************']
+for i in intro_msg:
+    print(i)
+   
+print('')
+
+menu=[
+    {
+        "name":"Appetizers",
+        "items":['Wings','Cookies','Spring Rolls'],
+    },
+        {
+        "name":"Entrees",
+        "items":['Salmon','Steak','Meat Tornado','A Literal Garden'],
+    },
+        {
+        "name":"Desserts",
+        "items":['Ice Cream','Cake','Pie'],
+    },
+        {
+        "name":"Drinks",
+        "items":['Coffee','Tea','latte'],
+    }
+]
+
+for i in menu:
+    print(i['name'])
+    print('----------')
+    for x in i['items']:
+        print(x)
+    print('')
+orders=[]
+
+
+while True:
+    order=input("""***********************************
+** What would you like to order? **
+***********************************
+    """)
+    order=order.capitalize()
+    if order=='Quit':
+        break
+    else:
+        orders.append(order)
+        c=Counter(orders)
+        if c[order]==1:
+             print(f"** {c[order]} order of {order} added to your meal **")
+        else:
+            print(f"** {c[order]} orders of {order} added to your meal **")
+
+ 
+
+
+ 
